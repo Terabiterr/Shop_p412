0ae2f79a4b3ed6de78dd6adcf3b84a00cb4b026a
Mario <mario@gmail.com>
2026-01-27 20:32:07 +0200

    Add project files.

---
+++ Shop_p412/Controllers/ProductsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Shop_p412.Controllers
+{
+    public class ProductsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
